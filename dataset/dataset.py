a9409eeea49ec3e875b3659b5943ff05ec323840
willi19 <willi19@snu.ac.kr>
2021-02-17 14:59:35 +0900

    dataloader

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -6,18 +6,18 @@ from utils import GenerateHeatmap
 from PIL import Image
 import os
 import numpy as np
+import hydra
+from hydra.utils import get_original_cwd
+import matplotlib.pyplot as plt
 
-class PoseDataset(Dataset):
-    def __init__(self, istest):
-        self.train_path = './train_imgs/'
-        self.test_path = './test_imgs/'
-        self.keypoint_path = './train_df.csv'
-        self.train_list = os.listdir(self.train_path)
-        self.test_list = os.listdir(self.test_path)
-        self.keypoints = pd.read_csv(self.keypoint_path).values     #index 0 is name of image
-        self.labels = pd.read_csv(self.keypoint_path).columns
-        self.istest = istest
+class KPDataset(Dataset):
+    def __init__(self, istest, cfg: DictConfig, db, kypt = None):
         
+        self.istest = istest
+        self.heatmap = GenerateHeatmap(cfg)
+    
+    def get_relative(self, path):
+        return os.path.join(get_original_cwd(),path)
     
     def get_image(self, img_name):
         if self.istest:
@@ -25,28 +25,60 @@ class PoseDataset(Dataset):
         else:
             return np.asarray(Image.open(self.train_path+img_name))    
         
-    #def get_hitmap(self,)        
         
     def __getitem__(self, idx):
         if self.istest:
             return self.get_image(self.test_list[idx])
         else:
-            return self.get_image(self.train_list[idx]), self.keypoints[idx]
-        
+            return self.get_image(self.train_list[idx]), self.heatmap(self.keypoints[idx])
+    
+    def plot_img(self, idx):
+        img = self.get_image(self.train_list[idx])
+        hms = self.heatmap(self.keypoints[idx]).max(axis=0)
+        fig = plt.figure()
+        fig.add_subplot(2, 1, 1)
+        plt.imshow(hms)
+        fig.add_subplot(2, 1, 2)
+        plt.imshow(img)
+        plt.show()
          
-class DataModule(pl.LightningDataModule):
+class KPDataModule(pl.LightningDataModule):
     def __init__(self, cfg: DictConfig):
-        super(BaseDataModule, self).__init__()
+        super(DataModule, self).__init__()
         self.cfg = cfg
         self.batch_size = cfg.train.batch_size
+        self.num_workers = cfg.train.num_workers
+
+        self.train_path = self.get_relative('train_imgs/')
+        self.test_path = self.get_relative('test_imgs/')
+        self.keypoint_path = self.get_relative('train_df.csv')
+        
+        self.labels = pd.read_csv(self.keypoint_path).columns
+        
+        self.split_len = cfg.train.valid_split
+        train_list = os.listdir(self.train_path)[:self.split_len]
+        train_list.sort()
+
+        self.train_list = train_list[:self.split_len]
+        self.valid_list = train_list[self.split_len:]
+
+        self.test_list = os.listdir(self.test_path)
+        self.test_list.sort()
+
+
+        data = pd.read_csv(self.keypoint_path).values
+        self.keypoints = [[[d[2*i+1], d[2*i+2]] for i in range(cfg.data.num_keypoints)] for d in data]    #index 0 is name of image
+        self.train_keypoints = self.keypoints[:self.split_len]
+        self.test_keypoints = self.keypoints[self.split_len:]
+
 
     def prepare_data(self):
         pass
 
     def setup(self, stage=None):
-        self.train_dataset = None
-        self.val_dataset = None
-        self.test_dataset = None
+        self.train_dataset = KPDataset(False, cfg, self.train_list,self.train_keypoints)
+        self.val_dataset = KPDataset(False, cfg, self.valid_list,self.valid_keypoints)
+        self.test_dataset = KPDataset(True, cfg, self.test_list)
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size)
@@ -57,8 +89,11 @@ class DataModule(pl.LightningDataModule):
     def test_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=self.batch_size)
 
+@hydra.main(config_path=os.path.join('../config', 'config.yaml'), strict=False)
+def main(cfg: DictConfig):
+    p = PoseDataset(False, cfg) 
+    p.plot_img(0)
+    print(len(p.keypoints))
+
 if __name__ == '__main__':
-    p = PoseDataset(False)
-    print(p[0])
-    p = PoseDataset(True)
-    print(p[1].shape)
+    main()
--- dataset/utils.py
+++ dataset/utils.py
@@ -2,43 +2,48 @@ from omegaconf.dictconfig import DictConfig
 import numpy as np
 import pandas as pd
 import os
+import hydra
+import matplotlib.pyplot as plt
+
 
 class GenerateHeatmap():
     def __init__(self, cfg: DictConfig):
         self.num_parts = cfg.data.num_parts
         self.sigma = cfg.heatmap.sigma
-        size = 6*sigma + 3
+        size = 6*self.sigma + 3
         x = np.arange(0, size, 1, float)
         y = x[:, np.newaxis]
-        x0, y0 = 3*sigma + 1, 3*sigma + 1
-        self.g = np.exp(- ((x - x0) ** 2 + (y - y0) ** 2) / (2 * sigma ** 2))
+        x0, y0 = 3*self.sigma + 1, 3*self.sigma + 1
+        self.g = np.exp(- ((x - x0) ** 2 + (y - y0) ** 2) / (2 * self.sigma ** 2))
         self.H = cfg.heatmap.H
         self.W = cfg.heatmap.W
-        
 
     def __call__(self, keypoints):
-        hms = np.zeros(shape = (self.num_parts, self.output_res, self.output_res), dtype = np.float32)
+        hms = np.zeros(shape = (len(keypoints), self.H, self.W), dtype = np.float32)
         sigma = self.sigma
-        for p in keypoints:
-            for idx, pt in enumerate(p):
-                x, y = int(pt[0]), int(pt[1])
-                if x<0 or y<0 or x>=self.output_res or y>=self.output_res:
-                    continue
-                ul = int(x - 3*sigma - 1), int(y - 3*sigma - 1)
-                br = int(x + 3*sigma + 2), int(y + 3*sigma + 2)
+        for idx, pt in enumerate(keypoints):
+            x, y = int(pt[0]), int(pt[1])
+            if x<0 or y<0 or x>=self.W or y>=self.H:
+                continue
+            ul = int(x - 3*sigma - 1), int(y - 3*sigma - 1)
+            br = int(x + 3*sigma + 2), int(y + 3*sigma + 2)
 
-                c,d = max(0, -ul[0]), min(br[0], self.output_res) - ul[0]
-                a,b = max(0, -ul[1]), min(br[1], self.output_res) - ul[1]
+            c,d = max(0, -ul[0]), min(br[0], self.W) - ul[0]
+            a,b = max(0, -ul[1]), min(br[1], self.H) - ul[1]
 
-                cc,dd = max(0, ul[0]), min(br[0], self.output_res)
-                aa,bb = max(0, ul[1]), min(br[1], self.output_res)
-                hms[idx, aa:bb,cc:dd] = np.maximum(hms[idx, aa:bb,cc:dd], self.g[a:b,c:d])
+            cc,dd = max(0, ul[0]), min(br[0], self.W)
+            aa,bb = max(0, ul[1]), min(br[1], self.H)
+            hms[idx, aa:bb,cc:dd] = np.maximum(hms[idx, aa:bb,cc:dd], self.g[a:b,c:d])
         return hms
-    
+
+
+@hydra.main(config_path=os.path.join('../config', 'config.yaml'), strict=False)
+def main(cfg : DictConfig):
+    ghm = GenerateHeatmap(cfg)
+    heatmap = ghm([[[100,200]]])
+    plt.imshow(heatmap[0])
+    plt.show()
+
 
 if __name__ == '__main__':
-    print(os.getcwd())
-    df = pd.read_csv('./train_df.csv').values
-    head = pd.read_csv('./train_df.csv').columns
-    print(df[0])
-    print(head)
+    main()
